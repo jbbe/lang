ea4ab8dc4a46cfdd531c1e9bd004999c630e06ef
jbbe <jbbe@umich.edu>
2019-06-25 14:44:20 -0400

    finished chap2

--- 2-28.py
+++ 2-28.py
@@ -1,6 +1,6 @@
 import nltk
-from nltk.corpus import wn
-
+from nltk.corpus import wordnet as wn
+from collections import OrderedDict
 pairs = {'car': 'automobile',
          'gem': 'jewel',
          'journey': 'car',
@@ -24,3 +24,52 @@ pairs = {'car': 'automobile',
          'glass': 'magician', 
          'rooster': 'voyage', 
          'noon': 'string'}
+
+def similarity(a, b):
+    a_sets = []
+    b_sets = []
+    shortest_path = None
+    for synset in wn.synsets(a, wn.NOUN):
+        a_sets.append(synset)
+    
+    for synset in wn.synsets(b, wn.NOUN):
+        b_sets.append(synset)
+
+    for synset in wn.synsets(a, wn.VERB):
+        a_sets.append(synset)
+    
+    for synset in wn.synsets(b, wn.VERB):
+        b_sets.append(synset)
+
+    for synset in wn.synsets(a, wn.ADJ):
+        a_sets.append(synset)
+    
+    for synset in wn.synsets(b, wn.ADJ):
+        b_sets.append(synset)
+
+    for synset in wn.synsets(a, wn.ADV):
+        a_sets.append(synset)
+    
+    for synset in wn.synsets(b, wn.ADV):
+        b_sets.append(synset)
+    
+    for a_set in a_sets:
+        for b_set in b_sets:
+            sim = a_set.path_similarity(b_set)
+            if not sim:
+                continue
+            # print(sim)
+            if shortest_path is None:
+                shortest_path = sim
+            elif sim < shortest_path:
+                shortest_path = sim
+    
+
+    return shortest_path
+
+ranking = {}
+for pair in pairs:
+    ranking[pair] = similarity(pair[0], pair[1])
+
+for item in sorted(ranking.items(), key=lambda x:x[1]):
+    print(item)
